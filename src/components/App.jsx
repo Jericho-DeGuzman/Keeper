66e10d7ca3971f230f06c2d56a535015c65fb559
Jericho-DeGuzman <deguzmanjericho.sbr@gmail.com>
2023-08-20 14:05:17 +0800

    INITIAL PUSH

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import Note from "./Note.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+    return (
+        <div>   
+            <Heading />
+            <Note />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
+
